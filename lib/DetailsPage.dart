a142e0448752c20a9e131515703e43b93634c3c5
al Mohamady <al.Mohamady47@gmail.com>
2019-11-26 20:39:33 +0200

    ADD QTY BUTTONS

--- lib/DetailsPage.dart
+++ lib/DetailsPage.dart
@@ -84,6 +84,102 @@ class _DetailsPageState extends State<DetailsPage>{
                    ),
                    height: 200,
                    width: 200,
+                 ),
+                 Positioned(
+                   top: 250,
+                   left: 25,
+                   right: 25,
+                   child: Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     children: <Widget>[
+                       Text(widget.foodName,
+                         style: TextStyle(
+                           fontFamily: 'Montserrat',
+                           fontSize: 22,
+                           fontWeight: FontWeight.bold
+                         ),
+                       ),
+                       SizedBox(height: 20,),
+                       Row(
+                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                         children: <Widget>[
+                           Text(
+                             widget.foodPrice,
+                             style: TextStyle(
+                               fontFamily: 'Montserrat',
+                               fontSize: 20,
+                               color: Colors.grey
+                             ),
+                           ),
+                           Container(
+                             height: 25,
+                             width: 1.0,
+                             color: Colors.grey,
+                           ),
+                           Container(
+                             width: 125,
+                             height: 40,
+                             decoration: BoxDecoration(
+                               borderRadius: BorderRadius.circular(17),
+                               color: Color(0xFF7A9BEE),
+                             ),
+                             child: Row(
+                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                               children: <Widget>[
+                                 InkWell(
+                                   onTap: () {},
+                                   child: Container(
+                                     height: 25,
+                                     width: 25,
+                                     decoration: BoxDecoration(
+                                       borderRadius: BorderRadius.circular(7),
+                                       color: Color(0xFF7A9BEE)
+                                     ),
+                                     child: Center(
+                                       child: Icon(
+                                         Icons.remove,
+                                         color: Colors.white,
+                                         size: 20,
+                                       ),
+                                     ),
+                                   )
+                                 ),
+                                 Text( '1',
+                                   style: TextStyle(
+                                     color: Colors.white,
+                                     fontFamily: 'Montserrat',
+                                     fontSize: 15
+                                   ),
+                                 ),
+                                 InkWell(
+                                     onTap: () {},
+                                     child: Container(
+                                       height: 25,
+                                       width: 25,
+                                       decoration: BoxDecoration(
+                                           borderRadius: BorderRadius.circular(7),
+                                           color: Color(0xFF7A9BEE)
+                                       ),
+                                       child: Center(
+                                         child: Icon(
+                                           Icons.add,
+                                           color: Colors.white,
+                                           size: 20,
+                                         ),
+                                       ),
+                                     )
+                                 ),
+                               ],
+                             ),
+                           )
+
+                         ],
+
+                       )
+                     ],
+
+                   ),
+
                  )
             ],
           )
